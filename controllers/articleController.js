4d43dd1f9e127b68177c8853f85e8f41d0450489
rhsb <ovillafana65@gmail.com>
2023-03-28 11:36:26 -0700

    slide 25

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -9,3 +9,23 @@ module.exports.renderAddForm = function(req, res){
     };
     res.render('articles/add', {article});
 };
+module.exports.addArticle = async function(req, res){
+    console.log("Adding article now")
+    const article = await Article.create( {
+        title: req.body.title,
+        intro: req.body.intro,
+        image_url: req.body.image_url,
+        body: req.body.body,
+        author_id: 1, //todo get logged in user
+        published_on: new Date()
+    });
+    res.redirect('/') //todo change the redirect to view all once made
+};
+
+module.exports.displayArticle = async function(req, res){
+    const article = await Article.findByPk(req.params.articleId, {
+        include: ['author']
+    });
+    console.log(article)
+    res.render('articles/view', {article});
+};
--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.get('/article/add', articleController.renderAddForm);
+router.post('/article/add', articleController.addArticle);
+
+router.get('/article/:articleId', articleController.displayArticle);
 module.exports = router;
