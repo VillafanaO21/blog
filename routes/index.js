24aa926d111f44557627a3c52521eb6508d1c0fd
rhsb <ovillafana65@gmail.com>
2023-03-29 11:36:22 -0700

    slide 47

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,13 @@ const articleController = require('../controllers/articleController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/article');
 });
 router.get('/article/add', articleController.renderAddForm);
 router.post('/article/add', articleController.addArticle);
 
 router.get('/article/:articleId', articleController.displayArticle);
+router.get('/article', articleController.displayAll);
+router.get('/article/:articleId/edit', articleController.renderEditForm);
+router.post('/article/:articleId/edit', articleController.updateArticle);
 module.exports = router;
